63b204d8da41c7c2696a1579952494acf1b26cff
Marek Marik <marek.marik@ibm.com>
2024-11-10 15:46:02 +0100

    precompute_theta_pos_freqs

--- model.py
+++ model.py
@@ -22,6 +22,27 @@ class ModelArgs:
 
     device: str = None
 
+
+def precompute_theta_pos_freqs(head_dim: int, seq_len: int, device: str, theta: Optional[float]=10000.0):
+    assert head_dim % 2 == 0, 'DImension must be divisible by 2'
+    # Build the theta parameters
+    # According to the formula: theta_i = 10000 ^ (-2(i-1)/dim) for i = [1, 2 ... dim/2]
+    # Shape: (head_dim / 2)
+    theta_numerator = torch.arange(0, head_dim, 2).float()
+    # Shape: (head_dim / 2)
+    theta = 1.0 / (theta ** (theta_numerator / head_dim)).to(device)
+    # Construct the positions (the 'm' parameter)
+    # Shape: (seq_len)
+    m = torch.arange(seq_len, device=device)
+    # Multiply by each position using the outer product
+    # Shape: (seq_len) * (hed_dim / 2) -> (seq_len, head_dim / 2)
+    freqs = torch.outer(m, theta).float()
+    # We can compute complex numbers in the polar form c = R * exp(m * theta), where R = 1 as follows:
+    # (Seq_Len, Head_Dim / 2) -> (Seq_Len, Head_Dim / 2)
+    freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
+    return freqs_complex
+
+
  
 class Transformer(nn.Module):
     def __init__(self, args: ModelArgs) -> None:
